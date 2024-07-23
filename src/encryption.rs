c77e4f8d7241055dbe762c5a11b6d8762ab46e56
Arthur Meyre <arthur.meyre@zama.ai>
2024-07-23 18:21:35 +0200

    feat: simple GLWE encryption primitive

---
+++ src/encryption.rs
@@ -0,0 +1,100 @@
+use tfhe::core_crypto::commons::ciphertext_modulus::CiphertextModulusKind;
+use tfhe::core_crypto::commons::math::random::{Distribution, Uniform};
+use tfhe::core_crypto::prelude::*;
+
+pub fn encryption_delta<Scalar: UnsignedInteger>(
+    bits_reserved_for_computation: usize,
+    ciphertext_modulus: CiphertextModulus<Scalar>,
+) -> Scalar {
+    let modulus_for_computations = Scalar::ONE << bits_reserved_for_computation;
+    match ciphertext_modulus.kind() {
+        CiphertextModulusKind::Native => {
+            ((Scalar::ONE << (Scalar::BITS - 1)) / modulus_for_computations) << 1
+        }
+        CiphertextModulusKind::NonNativePowerOfTwo => {
+            let custom_mod = ciphertext_modulus
+                .get_custom_modulus_as_optional_scalar()
+                .unwrap();
+            custom_mod / modulus_for_computations
+        }
+        CiphertextModulusKind::Other => todo!("Only power of 2 moduli are supported"),
+    }
+}
+
+pub fn encrypt_slice_as_glwe<Scalar, KeyCont, NoiseDistribution>(
+    input: &[Scalar],
+    glwe_secret_key: &GlweSecretKey<KeyCont>,
+    bits_reserved_for_computation: usize,
+    noise_distribution: NoiseDistribution,
+    ciphertext_modulus: CiphertextModulus<Scalar>,
+    generator: &mut EncryptionRandomGenerator<ActivatedRandomGenerator>,
+) -> GlweCiphertextOwned<Scalar>
+where
+    NoiseDistribution: Distribution,
+    Scalar: UnsignedInteger + Encryptable<Uniform, NoiseDistribution>,
+    KeyCont: Container<Element = Scalar>,
+{
+    assert!(input.len() <= glwe_secret_key.polynomial_size().0);
+
+    let delta = encryption_delta(bits_reserved_for_computation, ciphertext_modulus);
+
+    let mut plaintext_list = PlaintextList::new(
+        Scalar::ZERO,
+        PlaintextCount(glwe_secret_key.polynomial_size().0),
+    );
+
+    for (plain, input) in plaintext_list.iter_mut().zip(input.iter()) {
+        *plain.0 = (*input) * delta;
+    }
+
+    let mut glwe = GlweCiphertext::new(
+        Scalar::ZERO,
+        glwe_secret_key.glwe_dimension().to_glwe_size(),
+        glwe_secret_key.polynomial_size(),
+        ciphertext_modulus,
+    );
+
+    encrypt_glwe_ciphertext(
+        &glwe_secret_key,
+        &mut glwe,
+        &plaintext_list,
+        noise_distribution,
+        generator,
+    );
+
+    glwe
+}
+
+pub fn decrypt_glwe<Scalar, InputCont, KeyCont>(
+    glwe_secret_key: &GlweSecretKey<KeyCont>,
+    glwe: &GlweCiphertext<InputCont>,
+    bits_reserved_for_computation: usize,
+) -> Vec<Scalar>
+where
+    Scalar: UnsignedTorus,
+    InputCont: Container<Element = Scalar>,
+    KeyCont: Container<Element = Scalar>,
+{
+    let mut decrypted = PlaintextList::new(Scalar::ZERO, PlaintextCount(glwe.polynomial_size().0));
+    let mut decoded = vec![Scalar::ZERO; decrypted.plaintext_count().0];
+    decrypt_glwe_ciphertext(glwe_secret_key, &glwe, &mut decrypted);
+
+    let ciphertext_modulus = glwe.ciphertext_modulus();
+    let delta = encryption_delta(bits_reserved_for_computation, ciphertext_modulus);
+
+    let decomposer = SignedDecomposer::new(
+        DecompositionBaseLog(
+            bits_reserved_for_computation
+                + ciphertext_modulus
+                    .get_power_of_two_scaling_to_native_torus()
+                    .ilog2() as usize,
+        ),
+        DecompositionLevelCount(1),
+    );
+
+    for (decoded_value, decrypted_value) in decoded.iter_mut().zip(decrypted.iter()) {
+        *decoded_value = decomposer.closest_representable(*decrypted_value.0) / delta;
+    }
+
+    decoded
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use tfhe::core_crypto::prelude::*;
+
+mod encryption;
+
 fn main() {
-    println!("Hello, world!");
+    let encryption_glwe_dimension = GlweDimension(1);
+    let polynomial_size = PolynomialSize(2048);
+    // TODO: For now no noise
+    let glwe_encryption_noise_distribution =
+        DynamicDistribution::new_gaussian_from_std_dev(StandardDev(0.0));
+
+    let ciphertext_modulus = CiphertextModulus::try_new_power_of_2(22).unwrap();
+    let bits_reserved_for_computation = 10;
+    let modulus_for_computation = 1u32 << bits_reserved_for_computation;
+
+    let data: Vec<u32> = (0..polynomial_size.0)
+        .map(|x| x as u32 % modulus_for_computation)
+        .collect();
+
+    let mut seeder = new_seeder();
+    let seeder = seeder.as_mut();
+    let mut secret_rng = SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+    let mut encryption_rng =
+        EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed(), seeder);
+
+    let glwe_secret_key = allocate_and_generate_new_binary_glwe_secret_key(
+        encryption_glwe_dimension,
+        polynomial_size,
+        &mut secret_rng,
+    );
+
+    let glwe = encryption::encrypt_slice_as_glwe(
+        &data,
+        &glwe_secret_key,
+        bits_reserved_for_computation,
+        glwe_encryption_noise_distribution,
+        ciphertext_modulus,
+        &mut encryption_rng,
+    );
+
+    let decrypted =
+        encryption::decrypt_glwe(&glwe_secret_key, &glwe, bits_reserved_for_computation);
+
+    assert_eq!(&decrypted, &data);
 }
