6ea9f6197edf20c89866a704494fc219961ee003
Andrei Stoian <andrei.stoian@zama.ai>
2024-09-13 17:53:25 +0100

    fix: reenable signed tests, change correctness check to manage negatives

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -8,28 +8,30 @@ import json
 @pytest.mark.parametrize("n_bits", [2, 6, 8])
 @pytest.mark.parametrize("dims", [1, 2])
 @pytest.mark.parametrize("inner_size", [256, 1024, 2048, 4096])
-@pytest.mark.parametrize("signed", [False]) #, True])
-def test_correctness(n_bits, inner_size, dims, signed, crypto_params):
-    low = -2**(n_bits-1) if signed else 0 # randint low value is included
-    high = 2**(n_bits-1) if signed else 2**n_bits # randint high value is not included
+@pytest.mark.parametrize("signed_b", [False, True])
+def test_correctness(n_bits, inner_size, dims, signed_b, crypto_params):
+    low_b = -2**(n_bits-1) if signed_b else 0 # randint low value is included
+    high_b = 2**(n_bits-1) if signed_b else 2**n_bits # randint high value is not included
+
+    high_a = 2**n_bits
 
     inner_size_a = 1 if dims == 2 else 0
     inner_size_b = inner_size if dims == 2 else None
     # Randint draws from [low, high).
 
     if dims == 1:
-        a = np.random.randint(low, high, size=(inner_size,), dtype=np.int64)
-        b = np.random.randint(low, high, size=(inner_size,), dtype=np.int64)
+        a = np.random.randint(0, high_a, size=(inner_size,), dtype=np.int64)
+        b = np.random.randint(low_b, high_b, size=(inner_size,), dtype=np.int64)
     else:                
-        a = np.random.randint(low, high, size=(inner_size_a, inner_size), dtype=np.int64)
-        b = np.random.randint(low, high, size=(inner_size,inner_size_b), dtype=np.int64)
+        a = np.random.randint(0, high_a, size=(inner_size_a, inner_size), dtype=np.int64)
+        b = np.random.randint(low_b, high_b, size=(inner_size,inner_size_b), dtype=np.int64)
 
     reference = a @ b
 
     a = a.astype(np.uint64)
     b = b.astype(np.uint64)
 
-    n_bits_compute = max(20, int(np.log2(np.max(np.abs(reference)))) + 1)
+    n_bits_compute = 26 #max(20, int(np.log2(np.max(np.abs(reference)))) + 1)
     params = json.loads(crypto_params.serialize())
     params["bits_reserved_for_computation"] = n_bits_compute
     modified_crypto_params = deai.MatmulCryptoParameters.deserialize(json.dumps(params))
@@ -45,7 +47,7 @@ def test_correctness(n_bits, inner_size, dims, signed, crypto_params):
         decrypted_result = deai.decrypt_matrix(
             matmul_result, pkey, crypto_params, num_valid_glwe_values_in_last_ciphertext=num_valid_glwe_values_in_last_ciphertext
         )
-        decrypted_result = decrypted_result.reshape(-1,)
+        decrypted_result = decrypted_result.reshape(-1,).astype(np.int64)
     else:
         ciphertext_a = deai.encrypt(pkey, modified_crypto_params, a)
 
@@ -63,19 +65,20 @@ def test_correctness(n_bits, inner_size, dims, signed, crypto_params):
         )
 
         # Decrypt result to compare to reference
-        decrypted_result = deai.decrypt(encrypted_result, pkey, modified_crypto_params, num_valid_glwe_values_in_last_ciphertext=1)[0]
-    
+        decrypted_result = deai.decrypt(encrypted_result, pkey, modified_crypto_params, num_valid_glwe_values_in_last_ciphertext=1)
+        decrypted_result = np.asarray(decrypted_result).astype(np.int64)[0]
+
     sign_bit = 1 if np.any(reference) < 0 else 0
     msbs_to_check = 12 if n_bits_compute > 12 else n_bits_compute
-    mask = ((2**(n_bits_compute + sign_bit)) - 1) - (2**(n_bits_compute - msbs_to_check) - 1)
-    high_bits = (decrypted_result & mask)
-    high_bits_reference = (reference.astype(np.int64) & mask).reshape(-1,)
+    shift_delta = 2**(n_bits_compute - msbs_to_check) 
+    high_bits = decrypted_result // shift_delta
+    high_bits_reference = reference.astype(np.int64) // shift_delta
 
     if dims == 2:
         assert inner_size_a <= 1
         # FIXME: we should not allow 15% of error, change when noise is fixed
         n_allow_err = max(inner_size, params["polynomial_size"]) * 0.15
-        diff = np.abs(high_bits_reference - high_bits) // (2**(n_bits_compute - msbs_to_check))
+        diff = np.abs(high_bits_reference - high_bits) #// (2**(n_bits_compute - msbs_to_check))
         assert(np.sum(diff == 0) > inner_size - n_allow_err)
         assert(np.sum(diff) < n_allow_err)
         assert(np.all(diff) <= 1)
