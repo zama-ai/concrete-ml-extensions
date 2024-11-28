482fd855f4a01c876dfab5ca773ecc3b07b84145
Andrei Stoian <andrei.stoian@zama.ai>
2024-11-28 17:30:47 +0100

    fix: gpu instance type

--- scripts/print_cuda_infos.sh
+++ scripts/print_cuda_infos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -c "import concrete_ml_extensions as fhext; print('CUDA enabled: ', fhext.is_cuda_enabled());"
+poetry run python -c "import concrete_ml_extensions as fhext; print('CUDA enabled: ', fhext.is_cuda_enabled());"
 
-python -c "import concrete_ml_extensions as fhext; print('CUDA available: ', fhext.is_cuda_available());"
+poetry run python -c "import concrete_ml_extensions as fhext; print('CUDA available: ', fhext.is_cuda_available());"
