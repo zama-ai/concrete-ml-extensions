7a7272a399d24bef2910ec8f3d1a0d04da146fde
Andrei Stoian <andrei.stoian@zama.ai>
2025-01-27 17:24:37 +0100

    feat: pir test

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -99,3 +99,89 @@ def test_correctness(n_bits, inner_size, dims, signed_b, crypto_params):
     )
 
     assert np.sum(diff == 0) >= inner_size - n_allow_err
+
+
+@pytest.mark.parametrize("n_bits", [8])
+@pytest.mark.parametrize("num_items_in_ds", [10000])
+@pytest.mark.parametrize("item_size", [100000])
+@pytest.mark.parametrize("num_queries", [10])
+def test_pir(n_bits, num_queries, num_items_in_ds, item_size, crypto_params):
+    low_b = 0  # randint low value is included
+    high_b = 2**n_bits
+    
+    # Signed values must be processed for the weights, so
+    # we generate signed int64. This is also used to compute
+    # the max bits 
+    a = np.zeros((num_queries, num_items_in_ds), dtype=np.uint64)
+    for idx in range(num_queries):
+        a[idx, np.random.randint(num_items_in_ds)] = 1
+
+    b = np.random.randint(
+        low_b, high_b, size=(num_items_in_ds, item_size), dtype=np.int64
+    )
+
+    # These computations used signed weights
+    reference = a @ b
+
+    max_value = np.max(np.abs(reference))
+    n_bits_compute = int(np.ceil(np.log2(max_value + 1)))
+
+    assert n_bits_compute <= 27, "n_bits_compute exceeds maximum allowed value"
+
+    # Change the encoding to push the inputs and the result
+    # as much as possible to the left in the MSBs
+    # in order to avoid noise corruption
+    params = json.loads(fhext.default_params()) #crypto_params.serialize())
+    params["bits_reserved_for_computation"] = (
+        n_bits_compute + 1
+    )  # +1 for sign bit if needed
+#    params["packing_ks_level"] = 1
+    modified_crypto_params = fhext.MatmulCryptoParameters.deserialize(json.dumps(params))
+
+    pkey, ckey = fhext.create_private_key(modified_crypto_params)
+
+    # Need to convert to uint64 since this is what is handled
+    # by the crypto
+    a = a.astype(CRYPTO_DTYPE)
+    b = b.astype(CRYPTO_DTYPE)
+
+    encrypted_matrix = fhext.encrypt_matrix(
+        pkey=pkey, crypto_params=modified_crypto_params, data=a
+    )
+    start_time = time.time()
+
+    matmul_result = fhext.matrix_multiplication(
+        encrypted_matrix=encrypted_matrix, data=b, compression_key=ckey
+    )
+
+    tot_server_time = time.time() - start_time
+    device = "cuda" if fhext.is_cuda_enabled() and fhext.is_cuda_available() else "cpu" 
+    print(f"Server time without serialization {tot_server_time}s on {device}. ", end='')
+
+    polynomial_size = params["polynomial_size"]
+    num_valid_glwe_values_in_last_ciphertext = (
+        item_size % polynomial_size or polynomial_size
+    )
+
+    decrypted_result = fhext.decrypt_matrix(
+        matmul_result,
+        pkey,
+        modified_crypto_params,
+        num_valid_glwe_values_in_last_ciphertext=num_valid_glwe_values_in_last_ciphertext,
+    )
+    decrypted_result = decrypted_result.astype(np.int64)
+
+    # Need to check only MSBS
+    # since these are those that are guaranteed
+    # to be correct by the crypto-parameters
+    msbs_to_check = n_bits
+    shift_delta = 2 ** (n_bits_compute - msbs_to_check)
+    high_bits = decrypted_result // shift_delta
+    high_bits_reference = reference.astype(np.int64) // shift_delta
+
+    n_allow_err = 0
+    diff = np.abs(
+        high_bits_reference - high_bits
+    )
+
+    assert np.sum(diff == 0) >= item_size - n_allow_err
