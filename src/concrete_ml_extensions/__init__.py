48a799e36ada14b29611513d12f957335f97f05e
Andrei Stoian <andrei.stoian@zama.ai>
2025-04-03 16:30:10 +0200

    fix: update version

--- src/concrete_ml_extensions/__init__.py
+++ src/concrete_ml_extensions/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "concrete-ml-extensions"
 __author__ = "Zama"
 __all__ = ["concrete-ml-extensions"]
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 from .concrete_ml_extensions import *
 
