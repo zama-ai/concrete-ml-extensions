7c80b7c4f1d64a3f7626afdb80cfaaf8c64aab74
Andrei Stoian <andrei.stoian@zama.ai>
2025-04-22 18:02:27 +0200

    feat: encrypt decrypt 16b

--- src/concrete_ml_extensions/__init__.py
+++ src/concrete_ml_extensions/__init__.py
@@ -37,7 +37,7 @@ def deserialize_compression_key(data):
 def encrypt_radix(arr: np.ndarray, secret_key: bytes) -> bytes:
     dtype = arr.dtype
 
-    if not arr.ndim == 2 or dtype.type not in (np.uint8, np.int8):
+    if not arr.ndim == 2 or dtype.type not in (np.uint8, np.int8, np.int16, np.uint16):
         raise AssertionError(
             f"Cannot encrypt datatype {str(dtype)} "
             f"to TFHE-rs serialized ciphertext, only 2-d [u]int8 ndarrays are supported"
@@ -47,6 +47,10 @@ def encrypt_radix(arr: np.ndarray, secret_key: bytes) -> bytes:
             return encrypt_serialize_u8_radix_2d(arr, secret_key)
         elif dtype.type is np.int8:
             return encrypt_serialize_i8_radix_2d(arr, secret_key)
+        elif dtype.type is np.uint16:
+            return encrypt_serialize_u16_radix_2d(arr, secret_key)
+        elif dtype.type is np.int16:
+            return encrypt_serialize_i16_radix_2d(arr, secret_key)
 
 
 def decrypt_radix(
